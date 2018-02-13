a765bff2f009c80e1f5dff9761aecbe7d6fcb08d
Adaming44 <Adaming44@192.168.1.145>
2018-02-13 11:46:06 +0100

    camille : test voyage dao

--- 01_Projet_Agence_BoVoyage/src/main/java/fr/adaming/dao/VoyageDaoImpl.java
+++ 01_Projet_Agence_BoVoyage/src/main/java/fr/adaming/dao/VoyageDaoImpl.java
@@ -2,6 +2,10 @@ package fr.adaming.dao;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
 import org.springframework.stereotype.Repository;
 
 import fr.adaming.model.Voyage;
@@ -9,29 +13,56 @@ import fr.adaming.model.Voyage;
 @Repository
 public class VoyageDaoImpl implements IVoyageDao {
 
-	@Override
-	public Voyage getVoyageById(int idVoyage) {
-		return null;
+	@PersistenceContext(unitName = "pu")
+	EntityManager em;
+
+	public void setEm(EntityManager em) {
+		this.em = em;
 	}
 
 	@Override
+	public Voyage getVoyageById(int idVoyage) {
+		return em.find(Voyage.class, idVoyage);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
 	public List<Voyage> getAllVoyages() {
-		return null;
+		// écriture de la requete JPQL
+		String req = "select v from Voyage as v";
+
+		// création d'un query
+		Query query = em.createQuery(req);
+
+		// retourner la liste
+		return query.getResultList();
 	}
 
 	@Override
 	public Voyage addVoyage(Voyage voy) {
-		return null;
+		em.persist(voy);
+		return voy;
 	}
 
 	@Override
 	public Voyage updateVoyage(Voyage voy) {
-		return null;
+		em.merge(voy);
+		return voy;
 	}
 
 	@Override
 	public int deleteVoyage(int idVoyage) {
-		return 0;
+		// écriture de la requete JPQL
+		String req = "delete from Voyage as v where v.id=:pId";
+
+		// création du query
+		Query query = em.createQuery(req);
+		
+		//assignation des paramètres
+		query.setParameter("pId", idVoyage);
+		
+		//envoi de la requete
+		return query.executeUpdate();
 	}
 
 }
