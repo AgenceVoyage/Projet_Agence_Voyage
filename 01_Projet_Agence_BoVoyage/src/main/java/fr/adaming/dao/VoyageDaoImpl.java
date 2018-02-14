c615e89dc0a469ea266d2c9f6410a2c12885f46f
Adaming44 <Adaming44@192.168.1.145>
2018-02-14 11:12:27 +0100

    camille : merge 2

--- 01_Projet_Agence_BoVoyage/src/main/java/fr/adaming/dao/VoyageDaoImpl.java
+++ 01_Projet_Agence_BoVoyage/src/main/java/fr/adaming/dao/VoyageDaoImpl.java
@@ -47,44 +47,43 @@ public class VoyageDaoImpl implements IVoyageDao {
 	@Override
 	public Voyage updateVoyage(Voyage voy) {
 		//écriture de la requete JPQL
-//		String req = "update Voyage as v set v.nom=:pNom,"
-//				+ "v.reference=:pRef, v.dateDepart=:pDateDepart,"
-//				+ "v.dateArrivee=:pDateArrivee, v.nbPlaces=:pNbPlaces,"
-//				+ "v.prixPublic=:pPrixPublic, v.remise=:pRemise,"
-//				+ "v.prixRemise=:pPrixRemise, v.disponible=:pDisponible, "
-//				+ "v.pays=:pPays, v.ville=:pVille,"
-//				+ "v.continent=:pContinent, v.compagnieVoyage=:pCompagnie, "
-//				+ "v.prestation=:pPrestation,"
-//				+ "v.voiture=:pVoiture, v.hotel=:pHotel,"
-//				+ "v.listeDossiers=:pDossiers where v.id=:pId";
-//		
-//		//écriture d'un query
-//		Query query = em.createQuery(req);
-//		
-//		//assignation des paramètres
-//		query.setParameter("pNom", voy.getNom());
-//		query.setParameter("pRef", voy.getReference());
-//		query.setParameter("pDateDepart", voy.getDateDepart());
-//		query.setParameter("pDateArrivee", voy.getDateArrivee());
-//		query.setParameter("pNbPlaces", voy.getNbPlaces());
-//		query.setParameter("pPrixPublic", voy.getPrixPublic());
-//		query.setParameter("pRemise", voy.getRemise());
-//		query.setParameter("pPrixRemise", voy.getPrixRemise());
-//		query.setParameter("pDisponible", voy.isDisponible());
-//		query.setParameter("pPays", voy.getPays());
-//		query.setParameter("pVille", voy.getVille());
-//		query.setParameter("pContinent", voy.getContinent());
-//		query.setParameter("pCompagnie", voy.getCompagnieVoyage());
-//		query.setParameter("pPrestation", voy.getPrestation());
-//		query.setParameter("pVoiture", voy.getVoiture());
-//		query.setParameter("pHotel", voy.getHotel());
-//		query.setParameter("pDossiers", voy.getListeDossiers());
-//		query.setParameter("pId", voy.getId());
-//		
-//		//envoi de la requete
-//		query.executeUpdate();
+		String req = "update Voyage as v set v.nom=:pNom,"
+				+ "v.reference=:pRef, v.dateDepart=:pDateDepart,"
+				+ "v.dateArrivee=:pDateArrivee, v.nbPlaces=:pNbPlaces,"
+				+ "v.prixPublic=:pPrixPublic, v.remise=:pRemise,"
+				+ "v.prixRemise=:pPrixRemise, v.disponible=:pDisponible, "
+				+ "v.pays=:pPays, v.ville=:pVille,"
+				+ "v.continent=:pContinent, v.compagnieVoyage=:pCompagnie, "
+				+ "v.prestation=:pPrestation,"
+				+ "v.voiture=:pVoiture, v.hotel=:pHotel,"
+				+ "v.listeDossiers=:pDossiers where v.id=:pId";
 		
-		em.merge(voy);
+		//écriture d'un query
+		Query query = em.createQuery(req);
+		
+		//assignation des paramètres
+		query.setParameter("pNom", voy.getNom());
+		query.setParameter("pRef", voy.getReference());
+		query.setParameter("pDateDepart", voy.getDateDepart());
+		query.setParameter("pDateArrivee", voy.getDateArrivee());
+		query.setParameter("pNbPlaces", voy.getNbPlaces());
+		query.setParameter("pPrixPublic", voy.getPrixPublic());
+		query.setParameter("pRemise", voy.getRemise());
+		query.setParameter("pPrixRemise", voy.getPrixRemise());
+		query.setParameter("pDisponible", voy.isDisponible());
+		query.setParameter("pPays", voy.getPays());
+		query.setParameter("pVille", voy.getVille());
+		query.setParameter("pContinent", voy.getContinent());
+		query.setParameter("pCompagnie", voy.getCompagnieVoyage());
+		query.setParameter("pPrestation", voy.getPrestation());
+		query.setParameter("pVoiture", voy.getVoiture());
+		query.setParameter("pHotel", voy.getHotel());
+		query.setParameter("pDossiers", voy.getListeDossiers());
+		query.setParameter("pId", voy.getId());
+		
+		//envoi de la requete
+		query.executeUpdate();
+		
 		return voy;
 	}
 
